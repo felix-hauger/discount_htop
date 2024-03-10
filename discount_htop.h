61d4558ca41069730edc95a1eba0a7d5356560c5
felix-hauger <felix.hauger@laplateforme.io>
2024-03-10 12:26:31 +0100

    Add header file
    Add header file discount_htop.h containing project imports & definitions.

---
+++ discount_htop.h
@@ -0,0 +1,13 @@
+#ifndef DISCOUNT_HTOP
+#define DISCOUNT_HTOP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <dirent.h>
+
+// Define functions here
+
+#endif
