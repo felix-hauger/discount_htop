f57b329e770694ecfcf528f7c726878c2cbc5fad
felix-hauger <felix.hauger@laplateforme.io>
2024-03-11 11:06:56 +0100

    Improve UI
    Improve ui with mvwprintw to print informations precisely where I want.

--- services.c
+++ services.c
@@ -18,6 +18,12 @@ void show_usage(WINDOW *win)
         exit(1);
     }
 
+    mvwprintw(win, 1, 0, "NAME");
+    mvwprintw(win, 1, 40, "PID");
+    mvwprintw(win, 1, 55, "PPID");
+    mvwprintw(win, 1, 75, "VM SIZE\n");
+    int i = 0;
+
     while ((dt = readdir(proc)) != NULL) {
         // current_processus = readdir(dt);
         // mvwprintw("%s\n", dt->d_name);
@@ -28,24 +34,33 @@ void show_usage(WINDOW *win)
         if (status != NULL) {
             while (fgets(line, sizeof(line), status)) {
                 // printf("Processus: %s\n", line);
-                if (my_strncmp(line, "Name:", 5) == 0 || my_strncmp(line, "Pid:", 4) == 0 || my_strncmp(line, "VmSize:", 7) == 0) {
+                if (my_strncmp(line, "Name:", 5) == 0) {
                     // printf("Processus: %s\n", line);
                     // wprintw(win, "%s", line);
                     int length = strlen(line);
                     if (length > 0 && line[length - 1] == '\n') {
                         line[length - 1] = '\0'; // Remove the newline character
                     }
-                    wprintw(win, "%s ", line);
+                    // wprintw(win, "%s ", line);
+                    mvwprintw(win, i+2, 0, "%s\t", line);
                     // while ((c = fgetc(line[])) != EOF) {
                     //     wprintw(win, "%c", c);
                     // }
-                    wprintw(win, " | ");
+                    // wprintw(win, " | ");
+                } else if (my_strncmp(line, "Pid:", 4) == 0) {
+                    mvwprintw(win, i+2, 40, "%s\t", line);
+                } else if (my_strncmp(line, "PPid:", 5) == 0) {
+                    mvwprintw(win, i+2, 55, "%s\t", line);
+                } else if (my_strncmp(line, "VmSize:", 7) == 0) {
+                    mvwprintw(win, i+2, 75, "%s\t", line);
                 }
+
             }
             wprintw(win, "\n");
 
             fclose(status);
-        }            
+            i++;
+        }        
     }
 
     closedir(proc);
